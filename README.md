d29df0107fc4541247189d9f6759b90fc4404135
Tin Lam <tinlam@gmail.com>
2015-03-15 08:39:15 -0500

    Initial checkin

---
+++ README.md
@@ -0,0 +1,6 @@
+MAC PDF Fix
+===========
+
+Run ```install.sh```.
+
+This is to replace the PDFKit in Yosemite with one that is in Maverick to address the Retina issues with font aliasing.
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/local/bin/bash
+
+SYSDIR=/System/Library/Frameworks/Quartz.framework/Versions/A/Frameworks/PDFKit.framework/Versions
+CURDIR="`pwd`"
+unzip A.zip
+cd $(SYSDIR)
+sudo mv A A_backup
+mv $(CURDIR)/A ./A
